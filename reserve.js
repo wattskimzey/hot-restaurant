323c1f8ade9757b2334308b7e8752c20244724ea
Weezer <ewint3@gmail.com>
2019-10-16 21:19:12 -0500

    added js files

---
+++ reserve.js
@@ -0,0 +1,9 @@
+var waitArray = [
+    {
+      customerName: "Ewin",
+      customerEmail: "Ewin@example.com",
+      customerID: "afhaque89",
+      phoneNumber: "111-222-3333"
+    }
+  ];
+  module.exports = waitArray;
